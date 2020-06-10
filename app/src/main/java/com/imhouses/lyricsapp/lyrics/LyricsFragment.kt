6ffd6e9796956cd60d929eec8f0e5867e9a0904f
Juan Casas <imhouses@ciencias.unam.mx>
2020-06-10 11:42:03 -0500

    Initialized project

---
+++ app/src/main/java/com/imhouses/lyricsapp/lyrics/LyricsFragment.kt
@@ -0,0 +1,18 @@
+package com.imhouses.lyricsapp.lyrics
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class LyricsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/imhouses/lyricsapp/search/SearchFragment.kt
@@ -0,0 +1,19 @@
+package com.imhouses.lyricsapp.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+}
