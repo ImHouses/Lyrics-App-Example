9cacb9181122a90cd52f26709b102f1431398131
Juan Casas <imhouses@ciencias.unam.mx>
2020-06-10 12:16:18 -0500

    Create Lyrics Search Activity

---
+++ app/src/main/java/com/imhouses/lyricsapp/ui/lyrics/LyricsSearchActivity.kt
@@ -0,0 +1,11 @@
+package com.imhouses.lyricsapp.ui.lyrics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LyricsSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lyrics_search)
+    }
+}
