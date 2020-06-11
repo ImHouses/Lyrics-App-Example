3daa32cce1ce002883b83b65e977cafa57a53f87
Juan Casas <imhouses@ciencias.unam.mx>
2020-06-11 01:25:54 -0500

    Improve Error Handling

--- app/src/main/java/com/imhouses/lyricsapp/framework/error/AppErrorHandler.kt
+++ app/src/main/java/com/imhouses/lyricsapp/framework/error/AppErrorHandler.kt
@@ -4,12 +4,15 @@ import com.imhouses.lyricsapp.domain.error.ErrorEntity
 import com.imhouses.lyricsapp.domain.error.ErrorHandler
 import retrofit2.HttpException
 import java.io.IOException
+import java.net.SocketTimeoutException
 
 class AppErrorHandler : ErrorHandler {
 
-    override fun getError(throwable: Throwable): ErrorEntity = when(throwable) {
-        is HttpException -> ErrorEntity.Network(throwable)
-        is IOException -> ErrorEntity.Network(throwable)
+    override fun getError(throwable: Throwable): ErrorEntity = when {
+        throwable is SocketTimeoutException -> ErrorEntity.ServiceUnavailable(throwable)
+        throwable is HttpException && throwable.code() == 404 -> ErrorEntity.ServiceUnavailable(throwable)
+        throwable is HttpException -> ErrorEntity.Network(throwable)
+        throwable is IOException -> ErrorEntity.Network(throwable)
         else -> ErrorEntity.Unknown(throwable)
     }
 }
